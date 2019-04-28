b2062a49317f50a6c915d74809b0c1893bc72950
Yura-by <s.older681@gmail.com>
2019-04-28 16:43:14 +0300

    до создания файла ctyle.css в build

--- gulpfile.js
+++ gulpfile.js
@@ -87,8 +87,7 @@ gulp.task("copy", function () {
   return gulp.src([
     "source/fonts/**/*.{woff,woff2}",
     "source/img/**",
-    "source/js/**",
-    "source/css/normalize.css"
+    "source/js/**"
     ], {
   base: "source"
   })
